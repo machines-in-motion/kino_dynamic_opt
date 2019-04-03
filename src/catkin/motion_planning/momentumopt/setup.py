87d955ebc60604dc072266f83514c2a15f88d856
Julian Viereck <jviereck@tuebingen.mpg.de>
2019-04-03 16:26:06 -0400

    Fix package definition in momentumopt/setup.py

--- src/catkin/motion_planning/momentumopt/setup.py
+++ src/catkin/motion_planning/momentumopt/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=[
-      'momentumopt'
+      'momentumopt',
       'momentumexe',
       'quadruped'
     ],
