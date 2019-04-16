a752811338019c8d2da6aee146d0dfafaa0ed9a5
Majid Khadiv <majidkhadiv@gmail.com>
2019-04-16 17:09:25 +0200

    a good trot motion with lqr gains to test

--- momentumopt/demos/PyDemoMomentumopt.py
+++ momentumopt/demos/PyDemoMomentumopt.py
@@ -270,10 +270,10 @@ def main(argv):
         kin_optimizer = motion_planner.kin_optimizer
         inv_kin = kin_optimizer.inv_kin
         etg = kin_optimizer.endeff_traj_generator
-        etg.z_offset = 0.05
+        etg.z_offset = 0.1
 
         inv_kin.w_com_tracking[:3] = 1.
-        inv_kin.w_com_tracking[3:] = 10.
+        inv_kin.w_com_tracking[3:] = 1.
         inv_kin.w_endeff_contact = 1.
         inv_kin.p_endeff_tracking = 1.
         inv_kin.p_com_tracking = 1.
