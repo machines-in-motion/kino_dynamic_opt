fixed bug in computation of cross product

momentumopt/src/momentumopt/kinoptpy/create_data_file.py
@@ -199,7 +199,7 @@ def create_lqr_files(time_vector, optimized_motion_eff, optimized_sequence, opti
     desired_pos_abs = interpolate("POSITION_ABSOLUTE", time_vector, optimized_motion_eff=optimized_motion_eff, optimized_sequence = optimized_sequence)
 
     max_time = 0 # time horizon in seconds
-    save_horizon = 4
+    save_horizon = 0
 
     if time_vector[-1] - int(time_vector[-1]) > 0.0:
         max_time = int(time_vector[-1]) + 1
@@ -298,14 +298,14 @@ def create_lqr_files(time_vector, optimized_motion_eff, optimized_sequence, opti
         des_forces*=robot_weight
         #print(desired_pos)
         print("saving trajectories....")
-        np.savetxt("quadruped_positions_eff.dat", des_positions_final)
-        np.savetxt("quadruped_velocities_eff.dat", des_velocities_final)
-        np.savetxt("quadruped_com_with_horizon.dat", des_com)
-        np.savetxt("quadruped_lmom_with_horizon.dat", des_lmom)
-        np.savetxt("quadruped_forces_with_horizon_part1.dat", des_forces[:,0:37])
-        np.savetxt("quadruped_forces_with_horizon_part2.dat", des_forces[:,37:])
-        np.savetxt("quadruped_quaternio_with_horizon.dat", des_quaternion)
-        np.savetxt("quadruped_base_ang_velocities_with_horizon.dat", des_base_ang_velocities)
-        np.savetxt("quadruped_positions_abs_with_horizon_part1.dat", des_positions_abs[:,0:37])
-        np.savetxt("quadruped_positions_abs_with_horizon_part2.dat", des_positions_abs[:,37:])
-        np.savetxt("des_contact_activation_with_horizon.dat", des_contact_activation)
+        # np.savetxt("quadruped_positions_eff.dat", des_positions_final)
+        # np.savetxt("quadruped_velocities_eff.dat", des_velocities_final)
+        # np.savetxt("quadruped_com_with_horizon.dat", des_com)
+        # np.savetxt("quadruped_lmom_with_horizon.dat", des_lmom)
+        # np.savetxt("quadruped_forces_with_horizon_part1.dat", des_forces[:,0:37])
+        # np.savetxt("quadruped_forces_with_horizon_part2.dat", des_forces[:,37:])
+        # np.savetxt("quadruped_quaternio_with_horizon.dat", des_quaternion)
+        # np.savetxt("quadruped_base_ang_velocities_with_horizon.dat", des_base_ang_velocities)
+        np.savetxt("quadruped_positions_abs_with_horizon_part.dat", des_positions_abs)
+        # np.savetxt("quadruped_positions_abs_with_horizon_part2.dat", des_positions_abs[:,37:])
+        # np.savetxt("des_contact_activation_with_horizon.dat", des_contact_activation)

momentumopt/src/momentumopt/kinoptpy/lqr_gain_computation.py
@@ -177,7 +177,7 @@ class end_effector_lqr:
         self.com_ang_vel = np.loadtxt(dir + "/quadruped_base_ang_velocities.dat", dtype=float)[:, [1,2,3]]
 
         self.end_eff_forces = np.loadtxt(dir + "/quadruped_forces.dat", dtype=float)[:, 1:]
-        # self.end_eff_abs_pos = np.loadtxt(dir + "/quadruped_forces.dat", dtype=float)[:, 1:])
+        self.end_eff_abs_pos = np.loadtxt(dir + "/quadruped_positions_abs_with_horizon_part.dat", dtype=float)[:, 1:]
 
         self.delta = 0.000001
         self.dt = 0.001
@@ -187,11 +187,11 @@ class end_effector_lqr:
                                   [0.0, 0.0, 0.02476124]]
 
 
-    def compute_r_cross(self, rx, ry, rz):
+    def compute_r_cross(self, end_eff_abs_pos, com_pos):
 
-        r_cross_mat = [[0, -rz, ry],
-                       [rz, 0, -rx],
-                       [-ry, -rx, 0]]
+        r_cross_mat = [[0, -(end_eff_abs_pos[2] - com_pos[2]), (end_eff_abs_pos[1] - com_pos[1])],
+                       [(end_eff_abs_pos[2] - com_pos[2]), 0, -(end_eff_abs_pos[0] - com_pos[0])],
+                       [-(end_eff_abs_pos[1] - com_pos[1]), -(end_eff_abs_pos[0] - com_pos[0]), 0]]
 
         return r_cross_mat
 
@@ -214,10 +214,10 @@ class end_effector_lqr:
         inertia = np.matmul(np.matmul(np.transpose(rot_t),self.inertia_com_frame), rot_t)
         inv_inertia = inv(np.matrix(inertia))
 
-        r_cross_inv_inertia_fl = np.matmul(inv_inertia, self.compute_r_cross(0.2, 0.15, 0.0))
-        r_cross_inv_inertia_fr = np.matmul(inv_inertia, self.compute_r_cross(0.2, -0.15, 0.0))
-        r_cross_inv_inertia_hl = np.matmul(inv_inertia, self.compute_r_cross(-0.2, 0.15, 0.0))
-        r_cross_inv_inertia_hr = np.matmul(inv_inertia, self.compute_r_cross(-0.2, -0.15, 0.0))
+        r_cross_inv_inertia_fl = np.matmul(inv_inertia, self.compute_r_cross(self.end_eff_abs_pos[t][0:3], self.com_pos[t]))
+        r_cross_inv_inertia_fr = np.matmul(inv_inertia, self.compute_r_cross(self.end_eff_abs_pos[t][3:6], self.com_pos[t]))
+        r_cross_inv_inertia_hl = np.matmul(inv_inertia, self.compute_r_cross(self.end_eff_abs_pos[t][6:9], self.com_pos[t]))
+        r_cross_inv_inertia_hr = np.matmul(inv_inertia, self.compute_r_cross(self.end_eff_abs_pos[t][9:12], self.com_pos[t]))
 
         self.B_t = np.block([[np.zeros((3,3)), np.zeros((3,3)),np.zeros((3,3)), np.zeros((3,3))],
                             [(1/self.mass)*np.identity(3), (1/self.mass)*np.identity(3), (1/self.mass)*np.identity(3), (1/self.mass)*np.identity(3)],