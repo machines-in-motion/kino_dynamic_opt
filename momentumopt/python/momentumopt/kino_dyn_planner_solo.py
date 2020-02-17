e2fbe7e5b9334b298eff6dce00ccbfaa6dc25f74
Pierre Fernbach <pierre.fernbach@laas.fr>
2020-02-17 19:01:01 +0100

    [Python3] use relative import for quadruped package

--- momentumopt/python/momentumopt/kino_dyn_planner_solo.py
+++ momentumopt/python/momentumopt/kino_dyn_planner_solo.py
@@ -21,7 +21,7 @@ from momentumopt.motion_execution import MotionExecutor
 from momentumopt.kinoptpy.create_data_file import create_file, create_qp_files, create_lqr_files
 
 from momentumopt.motion_planner import MotionPlanner
-from quadruped.quadruped_wrapper import QuadrupedWrapper, Quadruped12Wrapper
+from .quadruped.quadruped_wrapper import QuadrupedWrapper, Quadruped12Wrapper
 
 import matplotlib.pyplot as plt
 
