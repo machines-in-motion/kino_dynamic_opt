9c15212228c75f74ef6d4ab7dbb4a62c9f7fa937
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-12-08 15:31:21 +0100

    update the find package GUROBI and IPTOPT by creating targets.

--- solver/cmake/solver.cmake
+++ solver/cmake/solver.cmake
@@ -2,5 +2,4 @@
 # additional configuration for real-time solver #
 #################################################
 
-set(CMAKE_CXX_FLAGS "-O3 ${CMAKE_CXX_FLAGS}")
 set(solver_DEFINITIONS "EIGEN_STACK_ALLOCATION_LIMIT=0")
