2085a0951c67273b7fb6ec2f0101fa990f45f576
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-12-10 11:37:50 +0100

    [solver] warning hunt

--- solver/tests/test_problems/TestProblem01.hpp
+++ solver/tests/test_problems/TestProblem01.hpp
@@ -31,7 +31,7 @@ namespace nlp_test_problems
       }
 
       // definition of problem box constraints
-      void getNlpBounds(int n_vars, int n_cons, double* x_l, double* x_u, double* g_l, double* g_u)
+      void getNlpBounds(int n_vars, int /*n_cons*/, double* x_l, double* x_u, double* g_l, double* g_u)
       {
         // lower and upper bounds on variables
         for (int id=0; id<n_vars; id++)
@@ -49,7 +49,7 @@ namespace nlp_test_problems
       }
 
       // definition of starting point
-      void getStartingPoint(int n_vars, double* x)
+      void getStartingPoint(int /*n_vars*/, double* x)
       {
     	    x[0] = 1.0;
     	    x[1] = 5.0;
@@ -58,13 +58,13 @@ namespace nlp_test_problems
       }
 
       // definition of objective function
-      double evaluateObjective(int n_vars, const double* x)
+      double evaluateObjective(int /*n_vars*/, const double* x)
       {
     	    return x[0]*x[3]*( x[0]+x[1]+x[2] ) + x[2];
       }
 
       // definition of constraints function
-      void evaluateConstraintsVector(int n_vars, int n_cons, const double* x, double* constraints)
+      void evaluateConstraintsVector(int /*n_vars*/, int /*n_cons*/, const double* x, double* constraints)
       {
     	    constraints[0] = x[0]*x[1]*x[2]*x[3];
     	    constraints[1] = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
--- solver/tests/test_problems/TestProblem02.hpp
+++ solver/tests/test_problems/TestProblem02.hpp
@@ -31,7 +31,7 @@ namespace nlp_test_problems
       }
 
       // definition of problem box constraints
-      void getNlpBounds(int n_vars, int n_cons, double* x_l, double* x_u, double* g_l, double* g_u)
+      void getNlpBounds(int n_vars, int /*n_cons*/, double* x_l, double* x_u, double* /*g_l*/, double* /*g_u*/)
       {
     	    for (int i=0; i<n_vars; i++)
         {
@@ -41,20 +41,20 @@ namespace nlp_test_problems
       }
 
       // definition of starting point
-      void getStartingPoint(int n_vars, double* x)
+      void getStartingPoint(int /*n_vars*/, double* x)
       {
         x[0] =  0.0;
         x[1] = -1.0;
       }
 
       // definition of objective function
-      double evaluateObjective(int n_vars, const double* x)
+      double evaluateObjective(int /*n_vars*/, const double* x)
       {
         return 4.*pow(x[0],2.) - 2.1*pow(x[0],4.)+ 1./3.*pow(x[0],6.) + x[0]*x[1] - 4.*pow(x[1],2.) + 4.*pow(x[1],4.);
       }
 
       // definition of constraints function
-      void evaluateConstraintsVector(int n_vars, int n_cons, const double* x, double* constraints)
+      void evaluateConstraintsVector(int /*n_vars*/, int /*n_cons*/, const double* /*x*/, double* /*constraints*/)
       {
       }
   };
